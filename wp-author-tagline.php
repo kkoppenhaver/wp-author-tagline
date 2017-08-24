d329c9e4882b411ed97e5dffe3928691260d79bf
Keanan Koppenhaver <keanan@howtogeek.com>
2017-08-23 20:44:26 -0500

    Initial plugin details

---
+++ wp-author-tagline.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name:       WP Author Tagline
+ * Plugin URI:        http://alphaparticle.com/
+ * Description:       Lets authors set their own tagline that can be displayed in any post using a shortcode
+ * Version:           1.0.0
+ * Author:            Keanan Koppenhaver
+ * Author URI:        http://alphaparticle.com.com/
+ * License:           MIT
+ * License URI:       
+ * Text Domain:       wp-author-tagline
+ */

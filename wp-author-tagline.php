7ed37e54b0b94ff94f4e654fb4161882a0f49934
Keanan Koppenhaver <keanan@howtogeek.com>
2017-08-23 22:06:41 -0500

    Create table on plugin activation

--- wp-author-tagline.php
+++ wp-author-tagline.php
@@ -8,6 +8,25 @@
  * Author:            Keanan Koppenhaver
  * Author URI:        http://alphaparticle.com.com/
  * License:           MIT
- * License URI:       
+ * License URI:       https://github.com/kkoppenhaver/wp-author-tagline/blob/master/LICENSE
  * Text Domain:       wp-author-tagline
  */
+
+function wpat_install() {
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . 'taglines';
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		author mediumint(9) NOT NULL,
+		tagline text NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collate;";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+}
+
+register_activation_hook( __FILE__, 'wpat_install' );
